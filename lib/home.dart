170339130bc952d4b0512a67c243c5cc68d7370c
SeniorZhai <developer.zhaitao@gmail.com>
2019-06-06 17:41:45 +0800

    Tile grid

--- lib/home.dart
+++ lib/home.dart
@@ -6,7 +6,62 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<List<int>> grid = [];
+  List<List<int>> gridNew = [];
   int score = 0;
+  List<Widget> getGrid(double width, double height) {
+    List<Widget> grids = [];
+    for (int i = 0; i < 4; i++) {
+      for (int j = 0; j < 4; j++) {
+        int num = grid[i][j];
+        String number;
+        Color color;
+        if (num == 0) {
+          color = Color(0xFFbfafa0);
+          number = '';
+        } else if (num == 2 || num == 4) {
+          color = Color(0xFFeee4da);
+          number = '$num';
+        } else if (num == 8 || num == 64 || num == 256) {
+          color = Color(0xFFf5b27e);
+          number = '$num';
+        } else if (num == 16 || num == 32 || num == 1024) {
+          color = Color(0xFFecc402);
+          number = '$num';
+        } else if (num == 128 || num == 512) {
+          color = Color(0xFFf77b5f);
+          number = '$num';
+        } else {
+          color = Color(0xFF60d992);
+          number = '$num';
+        }
+        double size;
+        String n = "$number";
+        switch (n.length) {
+          case 1:
+          case 2:
+            size = 40.0;
+            break;
+          case 3:
+            size = 30.0;
+            break;
+          case 4:
+            size = 20;
+            break;
+        }
+        grids.add(Tile(number, width, height, color, size));
+      }
+    }
+    return grids;
+  }
+
+  @override
+  void initState() {
+    grid = blankGrid();
+    gridNew = blankGrid();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
@@ -64,6 +119,18 @@ class _HomePageState extends State<HomePage> {
               Container(
                 height: height,
                 color: Color(0xFFa2917d),
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: GestureDetector(
+                    child: GridView.count(
+                      primary: false,
+                      crossAxisSpacing: 10.0,
+                      mainAxisSpacing: 10.0,
+                      crossAxisCount: 4,
+                      children: getGrid(gridWidth, gridHeight),
+                    ),
+                  ),
+                ),
               ),
               Padding(
                 padding: EdgeInsets.all(10),
@@ -118,4 +185,47 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+}
+
+class Tile extends StatefulWidget {
+  String number;
+  double width, height;
+  Color color;
+  double size;
+  Tile(this.number, this.width, this.height, this.color, this.size);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _TileState();
+  }
+}
+
+class _TileState extends State<Tile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          widget.number,
+          style: TextStyle(
+              fontSize: widget.size,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFF766c62)),
+        ),
+      ),
+      width: widget.width,
+      height: widget.height,
+      decoration: BoxDecoration(
+          color: widget.color,
+          borderRadius: BorderRadius.all(Radius.circular(10.0))),
+    );
+  }
+}
+
+List<List<int>> blankGrid() {
+  List<List<int>> rows = [];
+  for (int i = 0; i < 4; i++) {
+    rows.add([0, 0, 0, 0]);
+  }
+  return rows;
 }
