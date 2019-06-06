2153698799d5e644a0b4add907c777dbd04d1f48
SeniorZhai <developer.zhaitao@gmail.com>
2019-06-06 16:56:12 +0800

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import './home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '2048',
+      theme: new ThemeData(primarySwatch: Colors.blue, fontFamily: 'StarJedi'),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
